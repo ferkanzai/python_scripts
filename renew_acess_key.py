755a9c332f0f6882bfad373efd2bfafdfed94734
Carmona Ayuela <fca@dca90494c924.ant.amazon.com>
2019-10-08 16:14:08 +0100

    latest version. still things to add

--- renew_acess_key.py
+++ renew_acess_key.py
@@ -1,15 +1,37 @@
 # This script will automatically change the Access and Secret key for all IAM users in the account if they're older than 90 days
 # In order to work, change the profile_name in line 10, to the one you want to check
 
-import boto3, json, datetime, platform, os
+# thing to add: STS call to know exactly which user the profile uses and only modify that one in the credentials file
+# if not, every user changes its credentials but the last one is kept in the file (and could be the wrong key)
+
+# add Windows credential file change
+
+import boto3, json, datetime, platform, os, re
 
 os_type = platform.system()
 now = datetime.datetime.now(datetime.timezone.utc)
 #print(now)
 
-session = boto3.Session(profile_name='personal')
-iam = session.client('iam')
+with open('/Users/fca/.aws/credentials') as creds:
+    lines = str(creds.readlines())
+    #print(lines)
+    profiles = re.findall('\[([A-Za-z]*)\]', lines)
 
+print('Profiles configured in this laptop: {}'.format(profiles))
+
+while True:
+    profile = input('Which profile you want to change credentials for?\n')
+    if profile not in profiles:
+        print('not a valid profile, try again\n')
+    else:
+        break
+
+session = boto3.Session(profile_name=profile)
+iam = session.client('iam')
+sts = session.client('sts')
+
+user_profile = sts.get_caller_identity()['Arn'].split('/')[1]
+#print(user_profile)
 users = [user['UserName'] for user in iam.list_users()['Users']]
 
 for user in users:
@@ -30,13 +52,14 @@ for user in users:
             )
             new_access_key = new_creds['AccessKey']['AccessKeyId']
             new_secret_key = new_creds['AccessKey']['SecretAccessKey']
-            os.system('aws configure set aws_access_key_id {} --profile personal'.format(new_access_key))
-            os.system('aws configure set aws_secret_access_key {} --profile personal'.format(new_secret_key))
-            print('Access key was changed to {} for personal profile'.format(new_access_key))
+            if user == user_profile:
+                os.system('aws configure set aws_access_key_id {} --profile {}'.format(new_access_key, profile))
+                os.system('aws configure set aws_secret_access_key {} --profile {}'.format(new_secret_key, profile))
+                print('Access key was changed to {} for {} profile'.format(new_access_key, profile))
             iam.delete_access_key(
                 UserName=user,
                 AccessKeyId=old_key
             )
             print('Old key {} was removed'.format(old_key))
         else:
-            print('Credentials are newer than 90 days, no changes were made')
+            print('Credentials for user {} are newer than 90 days, no changes were made'.format(user))
